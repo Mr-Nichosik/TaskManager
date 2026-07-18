fe7247feac78dc4732febaabc9620afa6de9f67c
Mr Nichosik <asyamovilya@gmail.com>
2026-07-18 13:42:18 +0700

    add trash page to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    base: "/TaskManager/"
+    base: "/TaskManager/",
+
+    build:
+    {
+        rollupOptions:
+        {
+            input:
+            {
+                main: resolve(__dirname, "index.html"),
+                trash: resolve(__dirname, "trash.html"),
+            },
+        },
+    },
 });
